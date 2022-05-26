8562b11a9ebce56b7212a8bf92bbedcf8ac6e4d9
budnik2905 <budnik2905@gmail.com>
2022-05-26 18:19:11 +0300

    ввод вывод

---
+++ Урок 1/input_output.py
@@ -0,0 +1,32 @@
+#выводим одно слово
+print('Hello')
+#выводим несколько слов
+print('Hello,world!')
+#выводим любой набор символов
+print("iqwhfl $ dsflj jjfk")
+#выводим целое число
+print(100)
+#выводим число с плавающей точкой
+print(90.89)
+#выводим истину
+print(True)
+#выводим ложь
+print(False)
+#выводим(объект) None
+print(None)
+#сторока целиком
+print('Привет, меня зовут Кеша мне 2 года')
+#у нас есть 2 переменные
+name = 'Кеша'
+age = 2
+# как вывести строку вместе с переменными в терминал
+print(name, age)
+# /  sep функция сепаратор
+print(name, age, sep='/')
+print(name, age, sep='/;h')
+# end  перенос строки
+print(name)
+print(age)
+print('------------->')
+print(name, end=';')
+print(age, end=';')
