b65f6e3e969d7f6fac636633410a9b431fc7e1c6
budnik2905 <budnik2905@gmail.com>
2022-05-28 18:47:36 +0300

    индексация списков,квадратные скобки [ ]

---
+++ Урок 2/lesson1.1.py
@@ -0,0 +1,8 @@
+friend = 'Максим'
+
+first_letter = friend[0]
+print('Первая буква имени друга', first_letter)
+print('Тип 1 -го символа тоже строка: ',type(first_letter))
+print(friend[3])
+# индексация с конца
+print(friend[-1])
